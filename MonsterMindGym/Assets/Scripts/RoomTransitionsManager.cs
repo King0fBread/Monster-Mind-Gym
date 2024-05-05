979b86424db0533047ee1a1788e2ea39bdcae8fa
Nikita Glubokov <ITVenomancer@gmail.com>
2024-05-05 10:01:27 +0800

    Finish room transitions
    Functional interactive room transitions

--- MonsterMindGym/Assets/Scripts/RoomTransitionsManager.cs
+++ MonsterMindGym/Assets/Scripts/RoomTransitionsManager.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Properties;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class RoomTransitionsManager : MonoBehaviour
 {
     [SerializeField] private RoomInfo[] _rooms;
     [SerializeField] private int _initialRoomID;
+
+    [SerializeField] private TextMeshProUGUI _roomNameText;
+    [SerializeField] private Button[] _movementButtonsLeftRight = new Button[2];
+
     private RoomInfo _currentRoom;
     private Camera _camera;
 
@@ -25,6 +32,26 @@ public class RoomTransitionsManager : MonoBehaviour
     }
     private void Start()
     {
+        SetCurrentRoomComponents();
+    }
+    private void SetCurrentRoomComponents()
+    {
         _camera.transform.position = _currentRoom.GetCamTransformPosition();
+        _roomNameText.text = _currentRoom.GetRoomName();
+
+        for(int i = 0; i<_movementButtonsLeftRight.Length; i++)
+        {
+            _movementButtonsLeftRight[i].gameObject.SetActive(_currentRoom.GetAvailableRoomDirections()[i]);
+        }
+    }
+    public void MoveToLeftRoomByButton()
+    {
+        _currentRoom = _rooms[_currentRoom.GetRoomID() - 1];
+        SetCurrentRoomComponents();
+    }
+    public void MoveToRightRoomByButton()
+    {
+        _currentRoom = _rooms[_currentRoom.GetRoomID() + 1];
+        SetCurrentRoomComponents();
     }
 }
