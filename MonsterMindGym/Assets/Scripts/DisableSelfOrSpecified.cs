ec83bafe33ef3d530b751b6b3c9fae8407a3e92c
Nikita Glubokov <ITVenomancer@gmail.com>
2024-05-06 22:12:58 +0800

    Add training countdown
    Added a countdown before minigames

---
+++ MonsterMindGym/Assets/Scripts/DisableSelfOrSpecified.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableSelfOrSpecified : MonoBehaviour
+{
+    public GameObject objectToDisable;
+    public void DisableSelf()
+    {
+        gameObject.SetActive(false);
+    }
+    public void DisableSpecified()
+    {
+        objectToDisable.SetActive(false);
+    }
+}
