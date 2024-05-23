faee6921fa8f1f8a97c99f72c1056ac8ef5fbd54
Nikita Glubokov <ITVenomancer@gmail.com>
2024-05-23 20:06:29 +0800

    Additions to minigame manager
    Working on the manager, functional on area entrance/exit mechanic

--- MonsterMindGym/Assets/Scripts/Minigames/MinigamesManager.cs
+++ MonsterMindGym/Assets/Scripts/Minigames/MinigamesManager.cs
@@ -1,13 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinigamesManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _backgroundObject;
+    [SerializeField] private GameObject _currencyObject;
+    [SerializeField] private GameObject _getReadyScreenObject;
     [SerializeField] private GameObject[] _minigameObjects;
+
+
+    [SerializeField] private Button _startButton;
+
     private GameObject _currentMinigame;
-    public void InitiateRandomMinigame()
+    private void Awake()
+    {
+        _startButton.onClick.RemoveAllListeners();
+        _startButton.onClick.AddListener(InitiateRandomMinigame);
+    }
+    private void InitiateRandomMinigame()
     {
         _currentMinigame = _minigameObjects[Random.Range(0, _minigameObjects.Length)];
+
+        SetRelatedObjectsStates();
+    }
+    private void SetRelatedObjectsStates()
+    {
+        _startButton.gameObject.SetActive(false);
+        _getReadyScreenObject.SetActive(true);
+        _currencyObject.gameObject.SetActive(false);
+
+        _backgroundObject.gameObject.SetActive(true);
+        _currentMinigame.SetActive(true);
     }
 }
--- MonsterMindGym/Assets/Scripts/Rooms/TrainingRoomFA.cs
+++ MonsterMindGym/Assets/Scripts/Rooms/TrainingRoomFA.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrainingRoomFA : MonoBehaviour, IFunctionalArea
 {
+    [SerializeField] private Button _startMinigameButton;
     public void ExecuteMechanicOnAreaEntrance()
     {
-        
+        _startMinigameButton.gameObject.SetActive(true);
     }
     public void ExecuteMechanicOnAreaExit()
     {
-
+        _startMinigameButton.gameObject.SetActive(false);
     }
 }
