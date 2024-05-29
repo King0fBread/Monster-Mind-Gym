26e680e6cc8de80c653b4c0fc9dd94b43d1d9485
Nikita Glubokov <ITVenomancer@gmail.com>
2024-05-29 09:40:38 +0800

    Work on currency system

---
+++ MonsterMindGym/Assets/Scripts/Currency/CurrencyManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyManager : MonoBehaviour
+{
+    private int _coins;
+    private void Awake()
+    {
+        LoadCurrency();
+    }
+    public void AddCoins(int amount)
+    {
+        //minigame reward calc ref
+    }
+    public bool TrySpendCoins(int amount)
+    {
+
+        return true;
+    }
+    private void SaveCurrency()
+    {
+        PlayerPrefs.SetInt("Coins", _coins);
+    }
+    private void LoadCurrency()
+    {
+        PlayerPrefs.GetInt("Coins", 0);
+    }
+}
--- MonsterMindGym/Assets/Scripts/Rooms/ExhibitionRoomFA.cs
+++ MonsterMindGym/Assets/Scripts/Rooms/ExhibitionRoomFA.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExhibitionRoomFA : MonoBehaviour, IFunctionalArea
 {
+    [SerializeField] private Button _growMonsterButton;
     public void ExecuteMechanicOnAreaEntrance()
     {
-        
+        //check if conditions are met
+        _growMonsterButton.gameObject.SetActive(true);
     }
     public void ExecuteMechanicOnAreaExit()
     {
-
+        _growMonsterButton.gameObject.SetActive(false);
+        
     }
 }
